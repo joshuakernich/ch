f693c6e1640ee469ef42e664e4261c1122ddfa5b
Josh <joshua.kernich@gmail.com>
2024-04-30 11:02:23 +1000

    First version

---
+++ raw.js
@@ -0,0 +1,96 @@
+RAW = `org,sa-c,sa-i,wa-c,wa-i,nswact-c,nswact-i,qld-c,qld-i,tas-c,tas-i,nt-c,nt-i
+ACCAP Section,,,,,4,2,4,2,,,3,2
+ACDC Taskforce,,,,,1,1.5,1,1.5,,,,
+Aged Care Act Transitions and YIPRC,,,1,2,,,,,,,,
+Aged Care Assessments,4,3,,,2,2,4,2,,,,
+Aged care Communications and Change,3,2,,,4,3,,,4,2,,
+Aged Care Quality & Safety Commission,,,4,3,,,,,,,,
+Aged Care Workforce,3,3,3,2,,,,,4,3,2,2
+Alcohol and Other Drugs,2,2,1,2,,,,,1,1,,
+AN-ACC Policy and Funding,,,,,3,3,2,2,,,,
+Choice and Transparency,2,2,,,2,2,3,2,,,,
+Commonwelth Home Supported Places Program Management,,,,,3,3,3,2,,,,
+Communication and Change,,,,,,,3,3,,,1,3
+Consumer Engagement,4,2,,,,,,,2,2,3,2
+Consumer Engagement,,,1,2,,,,,,,,
+Consumer Support Section,,,,,1,2,3,2,,,,
+Data & Evidence Unit (Owen Carr),,,,,2,2,4,2,,,,
+Dementia Diversity & Design,4,2,2,2,,,2.5,1.5,2,2,,
+Dementia Training & Behaviour Support Section (DDDBranch),,,,,2,2,3,1,,,,
+Digital Reform,,,1,2,3,3,2.5,3,1,2,2,3
+Diversity and Inclusion Section (DDD Branch),,,,,,,4,1,,,2,2
+EPRB,4,2,2,2,4,2,4,2,2,2,3,2
+Family Chronic Disease and Preventative Health,2,1,,,,,,,,,,
+FBP,,,,,,,3,2,,,,
+Finance Business Partner,,,,,,,,,,,3,2
+First Nations AC Commissioner/Interim (IFNACC),4,3,,,4,3,3,2,,,,
+First Nations Aged Care,4,3,3,2,3,3,3,3,,,4,3
+First Nations Aged Care Policy (MWD),,,,,,,3,2,,,,
+First Nations Health Division,,,,,1,3,1.5,3,,,,
+First Nations Health Division - Partnerships,,,2,2,,,,,3,2,,
+First Nations Health Division - Primary Health Care,,,2,2,,,,,1,2,,
+First Nations Health Workforce,,,,,,,,,,,3,3
+FNHD FCDPH,,,,,,,,,,,3,3
+FNHD IRPIP,,,,,,,,,,,1,2
+FNHD PHC/CC Infrastructure / CPHC,,,,,,,,,,,3,3
+FNHD PHC/CC Sector Strengthening,,,,,,,,,,,4,3
+FNHD PPP PES section,,,,,,,,,,,3,2
+FNHD PPP Strategic Policy / IAHP Policy,,,,,,,,,,,1,2
+Funding Operations / aged Care Subsidies and supplements,,,,,,,3,2,,,,
+Funding Operations & Analysis,1,2,,,3,3,,,,,,
+Harmonisation,,,1,2,1,1,,,,,,
+Health Interface Branch,,,,,2,2,3,1.5,,,,
+Health Reform Taskforce Branch - National Health Reform Agreement Team,,,,,1,2,1,1,,,1,3
+Health Resourcing Branch - Health Workforce Section,,,,,,,,,,,1,3
+Health System Strategy Branch,,,,,,,,,,,1,3
+Health Workforce Division,,,,,2,2,1,1,,,,
+Home Care Reform Transition,1,3,1,3,,,,,,,,
+Home Support Operations,2,2,1,3,3,3,2,3,3,3,,
+HRD Residential Care Funding Reform,,,,,,,,,3,3,,
+IFNACC,,,,,,,,,3,3,,
+Interim First Nations Aged Care Commissioner,,,,,4,3,,,,,,
+ITC,,,,,,,3,3,,,2,2
+Learning & Development,,,,,,,,,,,2,2
+Legislative Reform,3,3,2,2,2,1,2,2,2,2,,
+MA Viability Support and Capability Section,,,,,,,4,3,4,3,,
+MAB,,,,,,,,,,,2,2.5
+Market Adjustments,4,3,3,3,4,3,4,3,,,,
+Mental Health,,,,,,,,,1,1,,
+Ministerial and Parliamentary services Branch,,,,,2,3,2,3,,,,
+MPS,,,,,2,2,3.5,2,,,2,2.5
+MWD Aged Care Capital Assistance,,,,,,,,,2,3,,
+MWD FNAC First Nations Programs,,,,,,,,,1,2,,
+MWD Rural & Remote & MPS,,,,,,,,,4,2,,
+Navigation and Access ,2,2,1,3,2,3,1.5,3,1,3,,
+New Aged Care Act Transition,3,3,2,1,,,,,,,,
+NSO,4,3,4,3,4,3,3,3,4,3,3,2
+Other State and Territory Branches,4,3,,,,,,,,,,
+PHN Branch,2,2,2,2,2,2,3,2,,,1,3
+Policy, Partnerships and Performance,4,2,,,,,,,,,,
+Primary Care and Community Control,3,2,,,,,,,,,1,3
+Primary Care Division,,,,,1,1,1,1,1,1,,
+Program Assurance,1,2,1,1,1,3,1,3,,,,
+QLD & NT Branch,,,,,,,4,3,,,,
+QLD and NT Branch,,,,,,,,,,,4,3
+Quality and assurance - new aged care act,,,,,,,,,2,3,,
+Recruitment,,,,,,,3,2,,,3,2
+Reform branch,,,,,,,,,,,1,3
+Reform Management,2,3,1,3,,,,,,,,
+Residential Care Funding Reform,3,2,1,2,2,2,2.5,2.5,,,,
+Rural and Remote Policy (Thin Markets Branch),,,,,,,3,3,,,,
+SAH,,,,,1,3,,,,,3,3
+SAH Transition,,,1,3,,,,,,,,
+SDAP,,,,,,,,,,,4,3
+State and Territory Offices,,,,,,,,,,,3,3
+STO Network,,,,,,,4,3,,,,
+Strengthening providers,1,3,1,1,1,1,,,,,,
+Structural Adjustment Strategy,2,2,,,1.5,3,,,,,,
+Support at Home Reforms,1,3,1,3,,,1,3,1,3,,
+Support at Home Transition,,,,,,,,,1,3,,
+TCP STRC,,,,,2,2,1,2,,,2,2.5
+Thin Markets (Innovation section?),4,3,3,3,4,3,4,3,,,2,2
+workplace sollutions (Tas?),,,,,,,,,,,4,2
+Workplace Solutions ACT,,,,,3,3,,,,,,
+Workplace Solutions Qld,,,,,,,4,2.5,,,,
+Design and Dementia Supports Section (DDDBranch),,,,,2,2,,,,,,
+Diversity and Inclusion Section (DDD Branch),,,,,4,1,,,,,,`
